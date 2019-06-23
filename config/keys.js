0323eded6db658ff944b8429dacb46b387f6ba7e
pausang <kokosangpi@gmail.com>
2019-06-23 17:43:44 -0500

    heroku connect

---
+++ config/keys.js
@@ -0,0 +1,1 @@
+module.exports = "mongodb://booksearch:password1@ds241977.mlab.com:41977/heroku_qg2m3n5d"
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const MONGODB_URI = require("./config/keys");
 
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
@@ -18,7 +19,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/googlebooksearch",
+  MONGODB_URI || "mongodb://localhost/googlebooksearch",
   {
     useCreateIndex: true,
     useNewUrlParser: true
